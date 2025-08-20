9e7343b7c1aaa0bb4052c54b2e9c40e62967d221
Luan Braga <luanbraga.lb@gmail.com>
2025-08-20 19:39:36 -0300

    implementa a autenticação OAuth 2.0 do Google

---
+++ src/main/java/com/medicamentos/preco_maximo_consumidor_medicamentos_api/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.medicamentos.preco_maximo_consumidor_medicamentos_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults());
+        return http.build();
+    }
+}
