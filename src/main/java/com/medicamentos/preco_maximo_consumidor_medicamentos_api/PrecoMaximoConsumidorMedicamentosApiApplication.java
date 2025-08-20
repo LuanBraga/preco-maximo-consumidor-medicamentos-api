9363330cc011991f011405e32d9d0ed8d41590e1
Luan Braga <luanbraga.lb@gmail.com>
2025-08-20 14:12:41 -0300

    Renomeia nome do diretorio example

--- src/main/java/com/medicamentos/preco_maximo_consumidor_medicamentos_api/PrecoMaximoConsumidorMedicamentosApiApplication.java
+++ src/main/java/com/medicamentos/preco_maximo_consumidor_medicamentos_api/PrecoMaximoConsumidorMedicamentosApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.preco_maximo_consumidor_medicamentos_api;
+package com.medicamentos.preco_maximo_consumidor_medicamentos_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/medicamentos/preco_maximo_consumidor_medicamentos_api/PrecoMaximoConsumidorMedicamentosApiApplicationTests.java
+++ src/test/java/com/medicamentos/preco_maximo_consumidor_medicamentos_api/PrecoMaximoConsumidorMedicamentosApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.preco_maximo_consumidor_medicamentos_api;
+package com.medicamentos.preco_maximo_consumidor_medicamentos_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
