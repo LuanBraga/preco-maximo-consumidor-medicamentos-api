e9e7a8eeab8d9fd051f79f85890f1e3ab7140198
Luan Braga <luanbraga.lb@gmail.com>
2025-08-25 21:07:18 -0300

    refactor: Remove CSRF disablement from SecurityConfig
    This commit removes the explicit disabling of CSRF protection in the `SecurityConfig` class. Spring Security handles CSRF protection automatically, making this configuration redundant.

--- src/main/java/com/medicamentos/preco_maximo_consumidor_medicamentos_api/config/SecurityConfig.java
+++ src/main/java/com/medicamentos/preco_maximo_consumidor_medicamentos_api/config/SecurityConfig.java
@@ -14,7 +14,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
